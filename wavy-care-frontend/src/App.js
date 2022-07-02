f196dd095bae5f24c1a3b86b2bdae36698b5bbcb
trevorichi <trevorichi@gmail.com>
2022-07-02 14:59:07 -0400

    Added some text animations to show squid lmao, not going to be stying

--- wavy-care-frontend/src/App.js
+++ wavy-care-frontend/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import AnimatedText from 'react-animated-text-content';
 
 function App() {
   return (
     <div className="App">
-     <h1>The Beginning of WavyCare</h1>
+  <AnimatedText
+    type="words" // animate words or chars
+    animation={{
+      x: '200px',
+      y: '-20px',
+      scale: 1.1,
+      ease: 'ease-in-out',
+    }}
+    animationType="float"
+    interval={0.06}
+    duration={0.8}
+    tag="h1"
+    className="animated-paragraph"
+    includeWhiteSpaces
+    threshold={0.1}
+    rootMargin="20%"
+  >
+    The Humble Beginning of Wavy Care
+  </AnimatedText>
     </div>
   );
 }
